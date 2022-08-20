62510efa6c2d4de9988f4e071984665c8ce3ebfd
J. Nick Koston <nick@koston.org>
2022-08-20 11:25:40 -1000

    fix: make sure the ble connection is disconnected if the operation is canceled (#60)

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -1,3 +1,8 @@
+import asyncio
+import contextlib
+from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
 from bleak_retry_connector import BLEDevice
 
 from yalexs_ble.lock import Lock
@@ -5,3 +10,33 @@ from yalexs_ble.lock import Lock
 
 def test_create_lock():
     Lock(lambda: BLEDevice("aa:bb:cc:dd:ee:ff", "lock"), "0800200c9a66", 1, "mylock")
+
+
+@pytest.mark.asyncio
+async def test_connection_canceled_on_disconnect():
+    disconnect_mock = AsyncMock()
+    mock_client = MagicMock(connected=True, disconnect=disconnect_mock)
+    lock = Lock(
+        lambda: BLEDevice("aa:bb:cc:dd:ee:ff", "lock", delegate=""),
+        "0800200c9a66",
+        1,
+        "mylock",
+    )
+    lock.client = mock_client
+    lock._disconnected_event = asyncio.Event()
+
+    async def connect_and_wait():
+        async with lock:
+            await asyncio.sleep(2)
+
+    with patch("yalexs_ble.lock.Lock.connect"):
+        task = asyncio.create_task(connect_and_wait())
+        await asyncio.sleep(0)
+        task.cancel()
+
+    with contextlib.suppress(asyncio.CancelledError):
+        await task
+
+    assert task.cancelled() is True
+
+    assert len(disconnect_mock.mock_calls) == 1
